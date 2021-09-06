c0735e3a84f5c2e4610f9c074df135d4d20d31e2
nqhuynh <nghiem.huynh@yale.edu>
2021-09-06 17:50:35 -0400

    neutralize skill data

--- data-raw/skill_07_dta.R
+++ data-raw/skill_07_dta.R
@@ -1,43 +1,43 @@
 ## code to prepare `skill_07_dta` dataset goes here
 
 ### retrieve data to doanh nghiep info 2007
-dn2007.file <- system.file(
-      "extdata",
-      "Stata_2007",
-      "dn2007.dta",
-      package = "VNFirmSurvey"
-)
+# dn2007.file <- system.file(
+#       "extdata",
+#       "Stata_2007",
+#       "dn2007.dta",
+#       package = "VNFirmSurvey"
+# )
+#
+# ### read the Stata files
+# if (file.exists(dn2007.file)){
+#       dn_2007_dta <- read_dta(dn2007.file)
+# }else{
+#       print("File dn2007.dta does not exist. Consider using InputData function to copy correct files ?InputData")
+# }
+#
+#
+# ### select columns
+#
+# skill_07_dta <- setDT(dn_2007_dta)[, .(svyear = 2007,
+#                                        tinh,
+#                                        macs,
+#                                        madn,
+#                                        ma_thue,
+#                                        #endyear_L = ldct11,
+#                                        total_L = ld13,
+#                                        PhD = ldct91,
+#                                        Masters = ldct101,
+#                                        Uni = ldct111,
+#                                        College = ldct121,
+#                                        Pro = ldct131,
+#                                        Lterm_voc = ldct141,
+#                                        Sterm_voc = ldct151,
+#                                        untrained = ldct161)]
+#
+#
+# ### Clean and create skill variables
+#
+# skill_07_dta <- clean_skill(skill_07_dta)
 
-### read the Stata files
-if (file.exists(dn2007.file)){
-      dn_2007_dta <- read_dta(dn2007.file)
-}else{
-      print("File dn2007.dta does not exist. Consider using InputData function to copy correct files ?InputData")
-}
 
-
-### select columns
-
-skill_07_dta <- setDT(dn_2007_dta)[, .(svyear = 2007,
-                                       tinh,
-                                       macs,
-                                       madn,
-                                       ma_thue,
-                                       #endyear_L = ldct11,
-                                       total_L = ld13,
-                                       PhD = ldct91,
-                                       Masters = ldct101,
-                                       Uni = ldct111,
-                                       College = ldct121,
-                                       Pro = ldct131,
-                                       Lterm_voc = ldct141,
-                                       Sterm_voc = ldct151,
-                                       untrained = ldct161)]
-
-
-### Clean and create skill variables
-
-skill_07_dta <- clean_skill(skill_07_dta)
-
-
-usethis::use_data(skill_07_dta, overwrite = TRUE)
+#usethis::use_data(skill_07_dta, overwrite = TRUE)
--- data-raw/skill_int_data.R
+++ data-raw/skill_int_data.R
@@ -1,35 +1,35 @@
 ## code to prepare `skill_int_data` dataset goes here
 
 ### retrieve data to doanh nghiep info 2001
-dn2001.file <- system.file(
-      "extdata",
-      "Stata_2001",
-      "dn2001.dta",
-      package = "VNFirmSurvey"
-)
-
-### read the Stata files
-dn_2001_dta <- read_dta(dn2001.file)
-
-### select variables in headquarters
-
-skill_01_dta <- setDT(dn_2001_dta)[, .(svyear = 2001,
-                                       tinh,
-                                       macs,
-                                       madn,
-                                       ma_thue,
-                                       total_L = ldc11, # Number of workers at HEADQUARTER
-                                       PhD = ldc21,
-                                       Masters = ldc31,
-                                       Uni = ldc41,
-                                       College = ldc51,
-                                       #Pro_secd = ldc61,
-                                       #technical = ldc71,
-                                       high_skill_tec = ldc81,
-                                       other_labor =ldc91)]
-
-skill_01_dta <- clean_skill(skill_01_dta)
-
-### save the dataframe as an .rda file in package/data/
-
-usethis::use_data(skill_01_dta, overwrite = TRUE)
+# dn2001.file <- system.file(
+#       "extdata",
+#       "Stata_2001",
+#       "dn2001.dta",
+#       package = "VNFirmSurvey"
+# )
+#
+# ### read the Stata files
+# dn_2001_dta <- read_dta(dn2001.file)
+#
+# ### select variables in headquarters
+#
+# skill_01_dta <- setDT(dn_2001_dta)[, .(svyear = 2001,
+#                                        tinh,
+#                                        macs,
+#                                        madn,
+#                                        ma_thue,
+#                                        total_L = ldc11, # Number of workers at HEADQUARTER
+#                                        PhD = ldc21,
+#                                        Masters = ldc31,
+#                                        Uni = ldc41,
+#                                        College = ldc51,
+#                                        #Pro_secd = ldc61,
+#                                        #technical = ldc71,
+#                                        high_skill_tec = ldc81,
+#                                        other_labor =ldc91)]
+#
+# skill_01_dta <- clean_skill(skill_01_dta)
+#
+# ### save the dataframe as an .rda file in package/data/
+#
+# usethis::use_data(skill_01_dta, overwrite = TRUE)

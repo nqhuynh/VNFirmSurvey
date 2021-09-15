5dc6b3fa87c24a7ffb71caae515e91238d473628
nqhuynh <nghiem.huynh@yale.edu>
2021-09-14 21:13:41 -0400

    create harmonized ownership

--- R/ownership_type.r
+++ R/ownership_type.r
@@ -1,5 +1,5 @@
 
-#' @title Firm ownership type 2001, 2004, and 2007.
+#' @title Firm harmonized-ownership type 2001, 2004, and 2007
 #' @description Input a list of enterprise dn files and
 #' get location data for firms 2001, 2004 and 2007
 #' @param dta_list raw data list dn from GSO
@@ -8,12 +8,13 @@
 #' @param years A vector of survey years that correspond to the raw data list dn from GSO dta_list
 #' @return Either a stored data in store_dir, or a cleaned data frame.  A data frame with  rows and  variables
 #' @details The list of data dn has to be ordered correctly to match with the years vector of survey years.
+#' The harmonization follows Mccaig et al (2020) Appendix Table 4: Ownership types by year.
 #' @rdname ownership
 #' @import data.table
 #' @export
 
 ownership <- function(dta_list,
-                        years = c(2001),
+                        years = c(2001, 2004, 2007),
                         store_dir){
 
       ### read the Stata files
@@ -22,67 +23,134 @@ ownership <- function(dta_list,
 
       ### select columns
 
-      owner_data <- mapply(function(x, y) x %>% dplyr::mutate(svyear = y),
+      owner_data <- mapply(function(x, y) x[, svyear := y],
                         dn_dta,
                         years, SIMPLIFY = F)
 
-      owner_data[[1]][lhdn == 14 , .((ft_von))]
-      owner_data[[1]][svyear == 2001, .(madn, lhdn, ft_von)
-                      ][, .N, by = lhdn
-                        ][order(lhdn)]
+      owner_data <- mapply(harmonize_ownership,
+                           dn_dta,
+                           years,
+                           SIMPLIFY = F)
 
-      # 1 - Central SOE
-      # 2 - Local SOE
-      # 3 - Collective
-      # 4 - Private enterprise
-      # 5 - Partnership company
-      # 6 - LLC with 1 state member
-      # 7 - Private LLC with 1 member
-      # 8 - JSC with state Use ft_von > 50
-      # 9 - Private LLC with 2+ members
-      # 10 - JSC with state capital
-      # 11 - JSC without state capital
-      # 12 - 100% foreign
-      # 13 - Foreign with state partner
-      # 14 - Foreign with other partner
-      geo_dta <- lapply(geo_dta, function(x)  setDT(x)[, .( svyear,
-                                                            macs, madn,
-                                                            ma_thue,
-                                                            ma_thue2,
-                                                            xa,
-                                                            huyen,
-                                                            tinh)])
+      owner_data <-
+         lapply(owner_data,
+                function(x)
+                   (x)[, .( svyear, macs, madn,
+                            ma_thue, ownership,
+                            simple_ownership = fcase(
+                               (ownership == "Central SOE" |
+                                  ownership == "Local SOE" |
+                                   ownership == "State LLC" |
+                                  ownership == "JSC or Private LLC with state capital>50%"), "state-owned",
+                               (ownership == "Collective" |
+                                   ownership == "Private enterprise" |
+                                   ownership == "Partnership company" |
+                                   ownership == "Private LLC" |
+                                   ownership == "JSC with state capital<50%" |
+                                   ownership == "JSC without state capital"), "private",
+                               (ownership == "100% foreign" |
+                                   ownership == "Foreign with state partner"|
+                                   ownership == "Foreign with other partner"), "foreign"
+                                       ))])
 
-      geo_dta <- data.table::rbindlist(geo_dta)
+      owner_data <- data.table::rbindlist(owner_data)
 
-      DataExplorer::update_columns(geo_dta,
-                                   c( "madn", "macs", "ma_thue",
-                                      "ma_thue2", "xa", "huyen", "tinh"), as.factor)
+      DataExplorer::update_columns(owner_data,
+                                   c( "madn", "macs", "ma_thue"), as.factor)
 
-      #DataExplorer::profile_missing(geo_dta)
-
-      ### If need be, Remove 1 missing xa, huyen.
-      #wage_dta <- stats::na.omit(wage_dta)
+      #DataExplorer::profile_missing(owner_data)
 
       ## Label variables
-      var_label(geo_dta) <- list(svyear = "Year",
-                                 tinh = "Province",
-                                 macs = "Firm ID",
-                                 madn = "Another firm ID?",
-                                 ma_thue = "Tax ID",
-                                 xa = "commune",
-                                 huyen = "district")
 
 
       if (!missing(store_dir)){
 
-            saveRDS(geo_dta,
+            saveRDS(owner_data,
                     file = store_dir)
 
             return(print(paste("Data is stored at "), store_dir))
       }else{
 
-            return(geo_dta)
+            return(owner_data)
       }
 
+}
+
+
+
+
+harmonize_ownership <- function(dta, svyear){
+   if (svyear == 2001)
+   { #ft_von
+      dta[, ownership := fcase(
+         lhdn == 1, "Central SOE",
+         lhdn == 2, "Local SOE",
+         lhdn == 3, "Collective",
+         lhdn == 4, "Private enterprise",
+         lhdn == 5, "Partnership company",
+         lhdn == 6, "State LLC",
+         lhdn == 7, "Private LLC",
+         #lhdn == 8, "",
+         lhdn == 9, "Private LLC",
+         #lhdn == 10, "State LLC",
+         lhdn == 11, "JSC without state capital",
+         lhdn == 12, "100% foreign",
+         lhdn == 13, "Foreign with state partner",
+         lhdn == 14, "Foreign with other partner"
+      )]
+   }else if (svyear == 2002)
+   {
+      dta[, ownership := fcase(
+         lhdn == 1, "Central SOE",
+         lhdn == 2, "Local SOE",
+         lhdn == 3, "Collective",
+         lhdn == 4, "Private enterprise",
+         lhdn == 5, "Partnership company",
+         lhdn == 6, "State LLC",
+         lhdn == 7, "State LLC",
+         lhdn == 8, "Private LLC",
+         lhdn == 9, "JSC or Private LLC with state capital>50%",
+         lhdn == 10, "JSC with state capital<50%",
+         lhdn == 11, "JSC without state capital",
+         lhdn == 12, "100% foreign",
+         lhdn == 13, "Foreign with state partner",
+         lhdn == 14, "Foreign with other partner"
+      )]
+   }else if (svyear == 2003 | svyear == 2004)
+   {
+      dta[, ownership := fcase(
+         lhdn == 1, "Central SOE",
+         lhdn == 2, "Local SOE",
+         lhdn == 3, "State LLC",
+         lhdn == 4, "State LLC",
+         lhdn == 5, "JSC or Private LLC with state capital>50%",
+         lhdn == 6, "Collective",
+         lhdn == 7, "Private enterprise",
+         lhdn == 8, "Partnership company",
+         lhdn == 9, "Private LLC",
+         lhdn == 10, "JSC without state capital",
+         lhdn == 11, "JSC with state capital<50%",
+         lhdn == 12, "100% foreign",
+         lhdn == 13, "Foreign with state partner",
+         lhdn == 14, "Foreign with other partner"
+      )]}else if (svyear > 2004 & svyear < 2011)
+      {
+         dta[, ownership := fcase(
+            lhdn == 1, "Central SOE",
+            lhdn == 2, "Local SOE",
+            lhdn == 3, "State LLC",
+            lhdn == 4, "State LLC",
+            lhdn == 5, "JSC or Private LLC with state capital>50%",
+            lhdn == 6, "Collective",
+            lhdn == 7, "Private enterprise",
+            lhdn == 8, "Partnership company",
+            lhdn == 9, "Private LLC",
+            lhdn == 10, "JSC without state capital",
+            lhdn == 11, "JSC with state capital<50%",
+            lhdn == 12, "100% foreign",
+            lhdn == 13, "Foreign with state partner",
+            lhdn == 14, "Foreign with other partner"
+         )]
+      }
+   return(dta)
 }

f1f0d50794df0c0a562df35b9239e4fa40e90693
nqhuynh <nghiem.huynh@yale.edu>
2021-09-15 21:01:44 -0400

    add sectors

--- R/firm_location.r
+++ R/firm_location.r
@@ -1,8 +1,8 @@
 
 
-#' @title Firm geographic location 2001, 2004, and 2007, Not harmonized
+#' @title Firm geographic location and sectors 2000-2011, Not harmonized
 #' @description Input a list of enterprise dn files and
-#' get location data for firms 2001, 2004 and 2007
+#' get location data for firms 2000-2011
 #' @param dta_list raw data list dn from GSO
 #' @param store_dir If provided a store_dir, then the output wage data frame will be stored there.
 #' Otherwise, output the cleaned data frame.
@@ -14,20 +14,12 @@
 #' @export
 
 getLocation <- function(dta_list,
-                        years = c(2001, 2004, 2007),
+                        years,
                         store_dir){
 
       ### read the Stata files
-      dn_dta <- lapply(dta_list, haven::read_dta)
+      dn_dta <- lapply(dta_list, function(x) haven::read_dta(x, encoding = "latin1"))
       dn_dta <- lapply(dn_dta, setDT)
-      # if (all(lapply(dta_list, (file.exists)))){
-      #
-      #
-      # }else{
-      #       print("File dn2001.file or dn2007.dta does not exist.
-      #       Consider using InputData function to copy correct files ?InputData")
-      # }
-
 
       ### select columns
 
@@ -35,21 +27,29 @@ getLocation <- function(dta_list,
                          dn_dta,
                         years, SIMPLIFY = F)
 
-      geo_dta <- lapply(geo_dta, function(x)  setDT(x)[, .( svyear,
+      geo_dta <- lapply(geo_dta, function(x) harmonize_sector(x,
+                                                crosswalk = here::here("inst", "extdata",
+                                                                 "vsic_2007_to_1993.xlsx")))
+
+      geo_dta <- lapply(geo_dta, function(x)    (x)[, .( svyear,
                                                               macs, madn,
                                                               ma_thue,
-                                                              ma_thue2,
+                                                              #ma_thue2,
                                                               xa,
                                                               huyen,
-                                                              tinh)])
+                                                              tinh,
+                                                            sector = fcase(
+                                                               svyear < 2007, nganh_kd,
+                                                               svyear >= 2007, vsis_93) )])
 
       geo_dta <- data.table::rbindlist(geo_dta)
 
       DataExplorer::update_columns(geo_dta,
                                    c( "madn", "macs", "ma_thue",
-                                     "ma_thue2", "xa", "huyen", "tinh"), as.factor)
+                                      "xa", "huyen", "tinh",
+                                     "sector"), as.factor)
 
-      #DataExplorer::profile_missing(geo_dta)
+      DataExplorer::profile_missing(geo_dta)
 
       ### If need be, Remove 1 missing xa, huyen.
       #wage_dta <- stats::na.omit(wage_dta)
@@ -76,3 +76,32 @@ getLocation <- function(dta_list,
       }
 
 }
+
+
+
+harmonize_sector <- function(dta,
+                             crosswalk){
+
+      crosswalk <- readxl::read_xlsx(crosswalk)
+      crosswalk <- setDT(crosswalk)[, .(vsis_07 = nganh_kd,
+                                        vsis_93 = nganh_cu)]
+      dta <- merge(dta, crosswalk,
+            all.x = TRUE,
+            by.x = "nganh_kd",
+            by.y = "vsis_07")
+      return(dta)
+}
+
+
+## Create harmonized products
+# get 2007-2010 data
+# sector_crosswalk<- rbindlist(list(geo_dta[[2]][, .(svyear, tennganhkd, nganh_kd, nganh_cu)],
+#                                   geo_dta[[3]][, .(svyear, tennganhkd, nganh_kd, nganh_cu)],
+#                                   #geo_dta[[4]][, .(svyear, tennganhkd, nganh_kd, nganh_cu)],
+#                                   geo_dta[[5]][, .(svyear, tennganhkd, nganh_kd, nganh_cu)]))[!is.na(nganh_cu) ][order(-nganh_cu)]
+#
+# temp <- sector_crosswalk[!duplicated(nganh_kd) ][order(-nganh_kd)]
+# temp_2 <- sector_crosswalk[!duplicated(nganh_cu) ][order(-nganh_cu)]
+#
+# writexl::write_xlsx(temp, here("inst", "extdata", "vsic_2007_to_1993.xlsx"))
+# writexl::write_xlsx(temp_2, here("inst", "extdata", "vsic_1993_to_2007.xlsx"))
